f5df19466c590aecfed6503a91e584247e9621e0
lexskop <lexskop@gmail.com>
2022-10-13 18:40:37 +0700

    Решена первая задачка

--- task1/Program.cs
+++ task1/Program.cs
@@ -9,3 +9,87 @@
 9 5 3 2
 8 4 4 2
 */
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(data);
+    Console.ResetColor();
+}
+
+int randomNum(int min, int max)
+{
+    int num = new Random().Next(min, max);
+    return num;
+}
+
+void printOriginalMatrix(int rows, int columns)
+{
+    printColorData($"Получили случайный двухмерный массив размером [{rows}, {columns}], -> ");
+    int[,] table = new int[rows, columns];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j] = randomNum(-10, 10)}\t");
+        }
+        Console.WriteLine("");
+    }
+    calcAndPrint(table);
+}
+
+void calcAndPrint(int[,] currentTable)
+{
+    printColorData("Получили новый массив -> ");
+    for (int i = 0; i < currentTable.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentTable.GetLength(1); j++)
+        {
+            int temp = currentTable[i, j];
+            int max = currentTable[i, j];
+            int indexMax = 0;
+            for (int count = 0; count + j < currentTable.GetLength(1); count++)
+            {
+                if (max < currentTable[i, j + count])
+                {
+                    max = currentTable[i, j + count];
+                    indexMax = j + count;
+                }
+            }
+            currentTable[i, j] = max;
+            currentTable[i, indexMax] = temp;
+            Console.Write($"{currentTable[i, j]}\t");
+        }
+        Console.WriteLine("");
+    }
+    userAnotherTry();
+}
+
+void userAnotherTry()
+{
+    Console.WriteLine("Вы хотите продолжить работу с программой? Да - Y, Нет - N");
+    string? userChoice = Console.ReadLine();
+    while (userChoice?.ToLower() != "y" && userChoice?.ToLower() != "n")
+    {
+        Console.WriteLine("Пожалуйста, введите верное решение. Если хотите продолжить работу - введите Y, если желаете закрыть программу - введите N");
+        userChoice = Console.ReadLine();
+    }
+    if (userChoice == "y")
+    {
+        userData();
+    }
+    else
+    {
+        Console.WriteLine("Bye!");
+    }
+}
+
+void userData()
+{
+    int userRows = randomNum(2, 5);
+    int userColumns = randomNum(2, 5);
+    printOriginalMatrix(userRows, userColumns);
+}
+
+Console.Clear();
+Console.WriteLine("Приветствую! Эта программа на вход принимает позицию элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.");
+userData();
